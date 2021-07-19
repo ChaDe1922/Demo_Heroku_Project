0e6eb2998ba66cfb40d019594f0acaa89174dec2
Chalece Delacoudray <cdelacoudray@codio.com>
2021-07-19 05:56:49 +0000

    added Flask Front End

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello, this is one more change to make sure I'm not crazy..."
-
-
-
-if __name__ == "__main__":
-       app.run(debug=True, host="0.0.0.0")
-
+def hello_world():
+    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    
+@app.route("/second_page")
+def second_page():
+    return render_template('second_page.html', subtitle='Second Page', text='This is the second page text')
+    
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
